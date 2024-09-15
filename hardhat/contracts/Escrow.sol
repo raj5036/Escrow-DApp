026f30e3a133d722cbd90cd598ba23cdf659a87e
Raj Karmakar <97rajmath@gmail.com>
2024-09-15 12:30:23 +0530

    Add Methods to send funds to address

--- hardhat/contracts/Escrow.sol
+++ hardhat/contracts/Escrow.sol
@@ -3,12 +3,20 @@ pragma solidity ^0.8.24;
 
 contract Escrow {
 	address private arbiter;
+	mapping(address => bool) public depostors;
 
 	error NotArbiter(address);
+	error InsufficientFunds();
+
+	event TransactionApproved(address indexed from, address indexed to, uint256 amount);
+	event DepositorAdded(address indexed depositor);
+	event FundsSent(address indexed beneficiary, uint256 amount);
 
 	constructor	() payable {
 		arbiter = msg.sender; // Initially the deployer is the arbiter
 	}
+
+	receive() external payable {}
 
 	modifier OnlyArbiter() {
 		if (msg.sender != arbiter) {
@@ -23,5 +31,17 @@ contract Escrow {
 
 	function getCurrentFundsAmount() public view OnlyArbiter returns (uint256) {
 		return address(this).balance;
+	}
+
+	function addDepositor(address depositor) external {
+		depostors[depositor] = true;
+		emit DepositorAdded(depositor);
+	}
+
+	function sendFundsToBeneficiary(address beneficiary, uint256 amount) external {
+		(bool success, ) = beneficiary.call{value: amount}("");
+		require(success, "Failed to send funds to beneficiary");
+
+		emit FundsSent(beneficiary, amount);
 	}
 }
